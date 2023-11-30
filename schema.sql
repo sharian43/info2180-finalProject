ccb123c177e946fcc715afe22a9ffef04882b518
Sharian Johnson <114778798+sharian43@users.noreply.github.com>
2023-11-30 16:47:07 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,41 +1,41 @@
--- schema.sql
+CREATE DATABASE schema;
 
--- Create users table
+-- Users Table
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL,
+    role VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- Create contacts table
+-- Contacts Table
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50),
+    title VARCHAR(255),
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
     telephone VARCHAR(20),
     company VARCHAR(255),
-    type VARCHAR('sales lead', 'support') NOT NULL,
+    type VARCHAR(50) CHECK (type IN ('sales lead', 'support')),
     assigned_to INT,
     created_by INT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (assigned_to) REFERENCES users(id),
+    FOREIGN KEY (created_by) REFERENCES users(id)
 );
 
--- Create notes table
+-- Notes Table
 CREATE TABLE notes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     contact_id INT,
     comment TEXT NOT NULL,
     created_by INT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (contact_id) REFERENCES contacts(id),
+    FOREIGN KEY (created_by) REFERENCES users(id)
 );
-
--- Insert a user with hashed password
-INSERT INTO users (firstname, lastname, email, password, role) 
-VALUES ('Admin', 'Project2', 'admin@project2.com', '$2a$12$3WE1VhzjyFOY8/dWQghy2e/jr2eZpSjJi2D3rxgQ/fnSkUCSSr7Im', 'admin');
