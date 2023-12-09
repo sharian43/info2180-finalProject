949e43ca5cdd8d2c8aca26d95efb0b0e004534c0
Kerice Forde <fordekerice@gmail.com>
2023-12-09 01:18:12 -0500

    Edited login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ session_start();
 
 // Replace these variables with your actual database credentials
 $host = 'localhost';
-$username = "new_user";
+$username = "Admin";
 $password = 'password123';
 $dbname = "dolphin_crm";
 
@@ -37,14 +37,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($stmt->num_rows > 0) {
             $stmt->bind_result($user_id, $hashed_password);
             $stmt->fetch();
+            
+            //echo "email $email";
 
             // Verify password
-            if (password_verify($password, $hashed_password)) {
+            if (password_verify($password, $hashed_password))  {
                 // Password is correct, start a session
                 $_SESSION["user_id"] = $user_id;
+                if ($email=="admin@project2.com"){ 
+                   
+                    header("Location: addUser.php"); 
+                    exit();
+                }
+              
                 header("Location: dashboard.php"); 
                 exit();
             } else {
+                
                 echo "Invalid email or password.";
             }
         } else {
@@ -66,38 +75,38 @@ if (isset($_GET['logout'])) {
 $conn->close();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dolphin CRM</title>
-    <link rel="stylesheet" href="addUser.css">
-    <script src="log.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Dolphin CRM</title>
+     <link rel="stylesheet" href="styles.css">
+     <script src="log.js"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+ </head>
+ <body>
     
-  <header>
-        <img src="dolphin.png" id="header-pic">
-        <h3>Dolphin CRM</h3>
-    </header>
+   <header>
+         <img src="dolphin.png" id="header-pic">
+         <h3>Dolphin CRM</h3>
+     </header>
     
-    <div id="login">
-        <form action="#" method="post">
-            <label for="email">Email:</label>
+     <div id="login">
+         <form action="#" method="post">
+             <label for="email">Email:</label>
             <input type="email" name="email" required><br>
     
-            <label for="password">Password:</label>
-            <input type="password" name="password" required><br>
+             <label for="password">Password:</label>
+             <input type="password" name="password" required><br>
     
-            <button type="submit"><i class="fas fa-lock"></i> Login</button>
-        </form>
-    </div>
+             <button type="submit"><i class="fas fa-lock"></i> Login</button>
+         </form>
+     </div>
     
-    <footer>
-        <p>Copyright &copy; 2022 Dolphin CRM.</p>
-    </footer>
+     <footer>
+         <p>Copyright &copy; 2022 Dolphin CRM.</p>
+     </footer>
     
-</body>
-</html>
+ </body>
+ </html>
